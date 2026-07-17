789b3b3159eb99009cb2463f17df64bf1640d397
Rodion Efremov <coderodd3@yandex.com>
2026-07-17 15:29:18 +0300

    Initial commit.

---
+++ bi_hash_table.h
@@ -0,0 +1,87 @@
+#ifndef IO_GITHUB_CODERODDE_C_BIDIRECTIONAL_HASH_TABLE_H
+#define IO_GITHUB_CODERODDE_C_BIDIRECTIONAL_HASH_TABLE_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+struct bidirectional_hash_table_key_value_pair
+{
+    void* key;
+    void* value;
+};
+
+struct bidirectional_hash_table_collision_tree_node
+{
+    struct bidirectional_hash_table_key_value_pair* key_value_pair;
+    struct bidirectional_hash_table_collision_tree_node* left;
+    struct bidirectional_hash_table_collision_tree_node* right;
+    struct bidirectional_hash_table_collision_tree_node* parent;
+};
+
+struct bidirectional_hash_table
+{
+    struct bidirectional_hash_table_collision_tree_node** collision_trees_forward;
+    struct bidirectional_hash_table_collision_tree_node** collision_trees_backward;
+    size_t (*hash_function_key)(void*);
+    size_t (*hash_function_value)(void*);
+    int (*compare_function_key)(void*, void*);
+    int (*compare_function_value)(void*, void*);
+    size_t capacity;
+    size_t size;
+    float load_factor_threshold;
+};
+
+/******************************************************************
+Initializes a bidirectional hash table with the specified capacity.
+******************************************************************/
+void bidirectional_hash_table_init(struct bidirectional_hash_table* table, 
+                                   size_t capacity,
+                                   float load_factor_threshold,
+                                   size_t (*hash_function_key)(void*),
+                                   size_t (*hash_function_value)(void*),
+                                   int (*compare_function_key)(void*),
+                                   int (*compare_function_value)(void*));
+
+/*****************************************************************
+Destroys a bidirectional hash table and frees all allocated memory.                                                     
+*****************************************************************/
+void bidirectional_hash_table_destroy(struct bidirectional_hash_table* table);
+
+/************************************************************************************
+Inserts a key-value pair into the bidirectional hash table. If the key-value mapping  
+exists, does nothing. If either the key or the value is already present in the table, 
+it will be replaced with the new mapping.                                             
+************************************************************************************/
+void bidirectional_hash_table_insert(struct bidirectional_hash_table* table, void* key, void* value);
+
+/*********************************************************************************
+Finds the value associated with the specified key in the bidirectional hash table.
+*********************************************************************************/
+void* bidirectional_hash_table_find_by_key(struct bidirectional_hash_table* table, void* key);
+
+/*********************************************************************************
+Finds the key associated with the specified value in the bidirectional hash table.
+*********************************************************************************/
+void* bidirectional_hash_table_find_by_value(struct bidirectional_hash_table* table, void* value);
+
+/**********************************************************************************************
+Removes the key-value pair associated with the specified key from the bidirectional hash table.
+**********************************************************************************************/
+bool bidirectional_hash_table_remove_by_key(struct bidirectional_hash_table* table, void* key);
+
+/************************************************************************************************
+Removes the key-value pair associated with the specified value from the bidirectional hash table.
+************************************************************************************************/
+bool bidirectional_hash_table_remove_by_value(struct bidirectional_hash_table* table, void* value);
+
+/****************************************************************************************
+Returns true if the bidirectional hash table contains the specified key, false otherwise.
+****************************************************************************************/
+bool bidirectional_hash_table_contains_key(struct bidirectional_hash_table* table, void* key);
+
+/******************************************************************************************
+Returns true if the bidirectional hash table contains the specified value, false otherwise.
+******************************************************************************************/
+bool bidirectional_hash_table_contains_value(struct bidirectional_hash_table* table, void* value);
+
+#endif // IO_GITHUB_CODERODDE_C_BIDIRECTIONAL_HASH_TABLE_H
